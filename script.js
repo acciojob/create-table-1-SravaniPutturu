44b7a823735dad0271d76b2c686e6c9c8aa6fc24
SravaniPutturu <117716430+SravaniPutturu@users.noreply.github.com>
2023-04-13 15:09:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
     //Write your code here
-  
+	var td1 = document.createElement('td');
+	var td2 = document.createElement('td');
+
+	td1.textContent = 'New cell1';
+	td2.textContent = 'New cell2';
+
+	var table = document.getElementById('sampleTable');
+	table.appendChild(td1)
+	table.appendChild(td2)
+	
   
 }
